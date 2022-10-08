66a5a8e755e0b64fde7eac154956554bbe2bdc5b
Purva sharma <purva08815294379@gmail.com>
2022-10-08 05:51:12 +0530

    feat: added components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./App.css";
-import { featuredProduct } from "./data";
+import { featuredProduct, latestProduct, trendingProduct,blogs } from "./data";
+import brandBanner from './Assets/brandBanner.png'
 import Nav from "./Components/Nav";
+import Footer from "./Components/Footer";
 import Landing from "./Components/Landing";
 import FeatureCard from "./Components/FeatureCard";
+import ProductCard from "./Components/ProductCard";
+import CategoryCard from "./Components/CategoryCard";
+import BlogCard from "./Components/BlogCard";
+
 
 function App() {
   return (
@@ -26,6 +32,92 @@ function App() {
           );
         })}
       </div>
+
+      {/* Latest Products */}
+      <h1 className="text-center color-primary v-gutter">Latest Products</h1>
+      <div className="text-center color-primary">
+        <span className="text-small p-1 product-link">New Arrival</span>
+        <span className="text-small p-1 product-link">Best Seller</span>
+        <span className="text-small p-1 product-link">Featured</span>
+        <span className="text-small p-1 product-link">Special Offer</span>
+      </div>
+
+      <div className="flex container flex-wrap">
+        {latestProduct.map((i) => {
+          return (
+            <ProductCard
+              name={i.productName}
+              img={i.productImg}
+              price={i.productPrice}
+              discount={i.discount}
+            />
+          );
+        })}
+      </div>
+      {/* Trending Products */}
+      <h1 className="text-center color-primary v-gutter">Trending Products</h1>
+      <div className="container flex flex-space-between">
+        {featuredProduct.map((item) => {
+          return (
+            <FeatureCard
+              name={item.productName}
+              img={item.productImg}
+              code={item.productCode}
+              price={item.productPrice}
+            />
+          );
+        })}
+      </div>
+
+      {/* Top Category */}
+      <h1 className="text-center color-primary v-gutter">Top Categories</h1>
+      <div className="container flex flex-space-between">
+        {trendingProduct.map((item) => {
+          return (
+            <CategoryCard
+              name={item.productName}
+              img={item.productImg}
+              price={item.productPrice}
+            />
+          );
+        })}
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      {/* NewsLetter */}
+      <div className="w-100 flex v-gutter flex-space-center align-item-center container-newsletter">
+        <div className="w-50 text-center mt-1">
+          <h2 className="text-md color-primary">Get Leatest Update By Subscribe 0ur Newslater</h2>
+          <button className="btn btn-secondary">Shop Now</button>
+        </div>
+      </div>
+
+      {/* Brand banner */}
+      <div className="v-gutter text-center">
+        <img src={brandBanner} width="650px"/>
+      </div>
+
+      {/* Latest Blog */}
+      <h1 className="text-center color-primary v-gutter">Latest Blogs</h1>
+      <div className="container flex flex-space-between">
+        {blogs.map((item) => {
+          return (
+            <BlogCard
+              title={item.title}
+              img={item.img}
+              author={item.author}
+              date={item.date}
+              desc={item.text}
+            />
+          );
+        })}
+      </div>
+
+      {/* Footer */}
+      <Footer/>
+
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -3,6 +3,27 @@ import fp2 from './Assets/products/fp2.png'
 import fp3 from './Assets/products/fp3.png'
 import fp4 from './Assets/products/fp4.png'
 
+import fp5 from './Assets/products/fp5.png'
+import fp6 from './Assets/products/fp6.png'
+import fp7 from './Assets/products/fp7.png'
+import fp8 from './Assets/products/fp8.png'
+import fp9 from './Assets/products/fp9.png'
+import fp10 from './Assets/products/fp10.png'
+
+import fp11 from './Assets/products/fp11.png'
+import fp12 from './Assets/products/fp12.png'
+import fp13 from './Assets/products/fp13.png'
+import fp14 from './Assets/products/fp14.png'
+
+import blog1 from './Assets/blog/blog1.png'
+import blog2 from './Assets/blog/blog2.png'
+import blog3 from './Assets/blog/blog3.png'
+
+
+
+
+
+
 
 export const featuredProduct = [
     {
@@ -34,4 +55,122 @@ export const featuredProduct = [
 
     },
 
+]
+
+export const latestProduct = [
+    {
+        productImg:fp5,
+        productName:'Cantilever chair',
+        productPrice:'42.00',
+        discount:'65.00'
+
+    },
+    {
+        productImg:fp6,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp7,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp8,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp9,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp10,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+]
+
+export const trendingProduct = [
+    {
+        productImg:fp11,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp12,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp13,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+    {
+        productImg:fp14,
+        productName:'Cantilever chair',
+        productCode:'Y523201',
+        productPrice:'42.00',
+        discount:'65.00'
+
+
+    },
+]
+
+export const blogs=[
+    {
+        author:"SaberAli",
+        date:"21 August,2020",
+        img:blog1,
+        title:'Top esssential Trends in 2021',
+        text:"More off this less hello samlande lied much over tightly circa horse taped mightly",
+    },
+    {
+        author:"SaberAli",
+        date:"21 August,2020",
+        img:blog2,
+        title:'Top esssential Trends in 2021',
+        text:"More off this less hello samlande lied much over tightly circa horse taped mightly",
+    },
+    {
+        author:"SaberAli",
+        date:"21 August,2020",
+        img:blog3,
+        title:'Top esssential Trends in 2021',
+        text:"More off this less hello samlande lied much over tightly circa horse taped mightly",
+    },
 ]
